c84d4e422e74662332ccfc1ca0601409a6271b08
Kendall <hayeskendall21@gmail.com>
2021-08-28 10:56:11 -0700

    fix

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.querySelector('.dna').addEventListener('click', () => {
+  window.location = 'https://hvacdna.com/';
+});
+
+document.querySelector('.weather').addEventListener('click', () => {
+  window.location = 'https://is-it-raining.netlify.app';
+});
+
+document.querySelector('.contact-validation').addEventListener('click', () => {
+  window.location = 'https://checkthiscontact.com';
+});
+
+console.log('connected');
